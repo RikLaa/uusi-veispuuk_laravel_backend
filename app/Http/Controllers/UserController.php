6d10349ed67f1dda4e21f0d0016efce78b2a2365
testi <m>
2017-03-17 11:04:30 +0200

    Muutoksia tunnilta, ensimmäiset toimivat controllerit

--- app/Http/Controllers/UserController.php
+++ app/Http/Controllers/UserController.php
@@ -12,8 +12,19 @@ class UserController extends Controller
 	$users = DB::select('select * from user');
 	return $users;
 	}
-    
-    
-    
+    //show user profile info
+    public function show() {
+		// return 'here is your one and only user';
+        $oneuser = DB::select('select userID, pictureURL, firstName, lastName
+        from user
+        where userID = 1');
+        return $oneuser;
+	}
+	//create a new user
+    public function create() {
+			$new = DB::select("INSERT INTO user (userRole, password, pictureURL, firstName, lastName, email, field, campus) VALUES
+            (3, 'salasana1', 'jotainURL', 'Kirsi', 'Kernel', 'Kirsi@gmail.com', 'Liiketalous', 'Rajakatu')");
+	        return $new;
+	}
     
 }
