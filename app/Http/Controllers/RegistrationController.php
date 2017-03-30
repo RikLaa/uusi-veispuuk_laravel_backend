a90f089241f067f2b3ca53fad909c734e69a2f03
testi <m>
2017-03-30 16:43:23 +0300

    auth methods is done not working yet

--- app/Http/Controllers/RegistrationController.php
+++ app/Http/Controllers/RegistrationController.php
@@ -8,6 +8,42 @@ class RegistrationController extends Controller
 {
     public function create()
     {
-
+      return view('sessions.create');
     }
+}
+
+public function store(){
+
+  // validation
+  $this -> validate(request(), [
+
+    'email' =>'required',
+    'password' =>'required',
+    'firstName' =>'required',
+    'lastName' =>'required',
+    'field' =>'required',
+    'campus' =>'required',
+
+  ]);
+
+  // create and save the user
+$user = User::create(request(['password', 'firstName', 'lastName', 'email', 'field', 'campus']));
+
+
+/*
+// sign in
+auth()->login($user);
+
+//redirect to the homepage
+return redirect()->home();
+
+    public function create() {
+
+		$new = DB::select("insert into users (userRole, password, pictureURL, firstName, lastName, email, field, campus) values
+    (1, 'salasana', '/var/pictures', 'etunimi', 'sukunimi', 'nimi@gmail.com', 'ict', 'dynamo')");
+    return 'i created new user';
+	}
+
+*/
+
 }
--- routes/web.php
+++ routes/web.php
@@ -22,13 +22,16 @@ Route::group(['prefix' => 'api'], function() {
     // Controller for handling the user requests
 	Route::resource('user', 'UserController');
 
+  // Authentication routes
+  Route::get('/register', 'RegistrationController@create');
+  Route::post('/register', 'RegistrationController@store');
+
+  Route::('/login', 'SessionsController@Create');
+  Route::('/logout', 'SessionsController@destroy');
+
 
 // EVERY API CALL/PATH BEFORE THIS LINE!
 });
 
 
 Route::get('/home', 'HomeController@index');
-
-// Authentication routes
-Route::get('/register', 'RegistrationController@create');
-Route::('/login', 'SessionsController@Create');
