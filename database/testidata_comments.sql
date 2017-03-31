c8bdcadbd54b9b8ffc3d54b6a7e43cefbe51e2ae
testi <m>
2017-03-31 09:09:16 +0300

    kommenttidata

--- database/testidata_comments.sql
+++ database/testidata_comments.sql
@@ -1,6 +1,18 @@
 INSERT INTO comments (postID, userID, content) values
-(1, 2, 'sisaltoa'), 
-(2, 1, 'sisaltoa'), 
-(1, 2, 'sisaltoa'), 
-(3, 1, 'sisaltoa'), 
-(2, 3, 'sisaltoa');
+(1, 2, 'Palasin juuri uudesta koulustani, sain tänään tietää, että minun ei tarvitse osallistua ollenkaan ruotsin kielen tunneille, sillä kielitaitoni on niin vahva! Jee, tosi hienoa, saan skipattua kuulemma koko kurssin! Asuin vuonna -98 Ruotsissa ja opiskelin ruotsin kieltä Uppsalan yliopistossa, joten minun ei tarvitse käydä perusasioita enää läpi. Sama juttu englannin kielessä, opettelen ainoastaan bisnes-sanastoa.'), 
+(2, 1, 'Minusta onkin tulossa uusi jymyjuttu syyskuun Hymy-lehteen ensi viikolla. Minusta otettiin Playboy-koekuvat vähän aikaa sitten, ja ne lähetetään Amerikan Playboylle tarjolle. '), 
+(1, 2, ' Kuvat ovat ”minun henkisiäni” – ne on toteutettu ”karkkimaassa” ja Barbie-tyylisesti. Tapasimme sisareni Julian kanssa suomalaisen Playboy-kuvaaja Jarmo Pohjaniemen Miamissa vuonna 2004 ja hän lähetteli minulle myös paljon meiliä.'), 
+(3, 1, ' Hän oli erittäin kiinnostunut meistä molemmista. Muistan että hän kehui meitä kauniiksi tytöiksi, mutta painoa olisi pitänyt kuulemma pudottaa.'), 
+(4, 2, 'Palasin juuri uudesta koulustani, sain tänään tietää, että minun ei tarvitse osallistua ollenkaan ruotsin kielen tunneille, sillä kielitaitoni on niin vahva! Jee, tosi hienoa, saan skipattua kuulemma koko kurssin! Asuin vuonna -98 Ruotsissa ja opiskelin ruotsin kieltä Uppsalan yliopistossa, joten minun ei tarvitse käydä perusasioita enää läpi. Sama juttu englannin kielessä, opettelen ainoastaan bisnes-sanastoa.'), 
+(5, 1, 'Minusta onkin tulossa uusi jymyjuttu syyskuun Hymy-lehteen ensi viikolla. Minusta otettiin Playboy-koekuvat vähän aikaa sitten, ja ne lähetetään Amerikan Playboylle tarjolle. '), 
+(6, 2, ' Kuvat ovat ”minun henkisiäni” – ne on toteutettu ”karkkimaassa” ja Barbie-tyylisesti. Tapasimme sisareni Julian kanssa suomalaisen Playboy-kuvaaja Jarmo Pohjaniemen Miamissa vuonna 2004 ja hän lähetteli minulle myös paljon meiliä.'), 
+(7, 1, ' Hän oli erittäin kiinnostunut meistä molemmista. Muistan että hän kehui meitä kauniiksi tytöiksi, mutta painoa olisi pitänyt kuulemma pudottaa.'), 
+(1, 2, 'Palasin juuri uudesta koulustani, sain tänään tietää, että minun ei tarvitse osallistua ollenkaan ruotsin kielen tunneille, sillä kielitaitoni on niin vahva! Jee, tosi hienoa, saan skipattua kuulemma koko kurssin! Asuin vuonna -98 Ruotsissa ja opiskelin ruotsin kieltä Uppsalan yliopistossa, joten minun ei tarvitse käydä perusasioita enää läpi. Sama juttu englannin kielessä, opettelen ainoastaan bisnes-sanastoa.'), 
+(2, 1, 'Minusta onkin tulossa uusi jymyjuttu syyskuun Hymy-lehteen ensi viikolla. Minusta otettiin Playboy-koekuvat vähän aikaa sitten, ja ne lähetetään Amerikan Playboylle tarjolle. '), 
+(1, 2, ' Kuvat ovat ”minun henkisiäni” – ne on toteutettu ”karkkimaassa” ja Barbie-tyylisesti. Tapasimme sisareni Julian kanssa suomalaisen Playboy-kuvaaja Jarmo Pohjaniemen Miamissa vuonna 2004 ja hän lähetteli minulle myös paljon meiliä.'), 
+(3, 1, ' Hän oli erittäin kiinnostunut meistä molemmista. Muistan että hän kehui meitä kauniiksi tytöiksi, mutta painoa olisi pitänyt kuulemma pudottaa.'), 
+(1, 2, 'Palasin juuri uudesta koulustani, sain tänään tietää, että minun ei tarvitse osallistua ollenkaan ruotsin kielen tunneille, sillä kielitaitoni on niin vahva! Jee, tosi hienoa, saan skipattua kuulemma koko kurssin! Asuin vuonna -98 Ruotsissa ja opiskelin ruotsin kieltä Uppsalan yliopistossa, joten minun ei tarvitse käydä perusasioita enää läpi. Sama juttu englannin kielessä, opettelen ainoastaan bisnes-sanastoa.'), 
+(7, 1, 'Minusta onkin tulossa uusi jymyjuttu syyskuun Hymy-lehteen ensi viikolla. Minusta otettiin Playboy-koekuvat vähän aikaa sitten, ja ne lähetetään Amerikan Playboylle tarjolle. '), 
+(8, 2, ' Kuvat ovat ”minun henkisiäni” – ne on toteutettu ”karkkimaassa” ja Barbie-tyylisesti. Tapasimme sisareni Julian kanssa suomalaisen Playboy-kuvaaja Jarmo Pohjaniemen Miamissa vuonna 2004 ja hän lähetteli minulle myös paljon meiliä.'), 
+(9, 1, ' Hän oli erittäin kiinnostunut meistä molemmista. Muistan että hän kehui meitä kauniiksi tytöiksi, mutta painoa olisi pitänyt kuulemma pudottaa.'), 
+(2, 3, 'Joillakin lehdillä on tosi tarkat rajat painon kanssa, ja emme menneet Playboyn pyytämiin koekuvauksiinkaan, kun he sanoivat, että se on ainoa mahdollisuus.');
