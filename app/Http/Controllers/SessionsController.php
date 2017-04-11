28e0debaaeeaeeb07cadc9daa96fe0b362d2fb4e
RikLaa <riku.laajala@gmail.com>
2017-04-11 12:31:50 +0300

    middleware works

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -17,10 +17,30 @@ class SessionsController extends Controller
       $password = $request['password'];
 
       if (Auth::attempt(['email' => $email, 'password' => $password])) {
-          return 'true';
-      } else {
-          return 'false';
-      }
+          
+          //Session::set('email', $email);
+          $checkSession = $request->session()->set('email', $email);
+          
+          if ($checkSession) {
+              
+              //return redirect('/api/posts');
+              return true;
+              
+          } else  {
+              
+              //return redirect('/api/login');
+              return false;
+              
+          }
 
-  }
+        }
+      
+      
+}
+    public function logout(Request $request){
+        
+        $request->session()->flush();
+        return redirect('/api/login');
+        
+    }
 }
