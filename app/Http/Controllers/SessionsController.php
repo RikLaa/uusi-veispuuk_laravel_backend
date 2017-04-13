c19988cbb380bdea88021c0db65a3d75b0033a4d
RikLaa <riku.laajala@gmail.com>
2017-04-13 13:04:48 +0000

    logout works now

--- app/Http/Controllers/SessionsController.php
+++ app/Http/Controllers/SessionsController.php
@@ -9,10 +9,6 @@ use Illuminate\Routing\Controller;
 
 class SessionsController extends Controller
 {
-  /*public function create()*/
-  //{
-    //return view('sessions.create');
-  /*}*/
 
   public function authenticate(Request $request) {
       $email = $request['email'];
@@ -20,24 +16,8 @@ class SessionsController extends Controller
       
       $checkUser = DB::table('users')->where('email', '=', $email)->first();
       
-      /*if(isset($checkUser)){
-          
-          echo 'gfdsgfdsggfs';
-      }else{
-          
-          echo 'false';
-      }*/
-      
       if (isset($checkUser)) {
           $checkPassword = $checkUser->password;
-          
-      
-          
-        /*  if (Auth::attempt(['email' => $email, 'password' => $password]))
-        {
-            echo 'asdfsa';
-        }
-          */
           
           
          if(password_verify($password, $checkPassword)){
@@ -49,41 +29,8 @@ class SessionsController extends Controller
               return 'false';
           }
           
-          
-          
-          /*if (Hash::check($password, $checkPassword)) {
-              echo 'login ok';
-          }*/
-              
-          //if (Auth::attempt([$checkUser->email => $email, $checkPassword => $password]))
-              
-          /*if (password_verify($password, $checkPassword)) {
-      
-                session(['email' => $email]);
-                //echo 'login yes';
-              return true;
-          }*/
-          //echo $password . '-******-' . $checkPassword;
-    //      echo typeof($password);
       } 
       
-        
-      
-       /* if ($checkSession) {
-              
-              //return redirect('/api/posts');
-              return 'true';
-              
-          } else  {
-              
-              //return redirect('/api/login');
-              //return false;
-              return 'false';
-          }*/
-      
-      
-      /*$request->session()->set('email', $email);
-      /**/
       
   }
     public function logout(Request $request){
@@ -91,31 +38,8 @@ class SessionsController extends Controller
         //Auth::logout();
         //return $request;
         $request->session()->flush();
-        //return redirect('/api/login');
+        return 'true';
         
     }
 }
 
-
-
-//if (Auth::attempt(['email' => $email, 'password' => $password])) {
-          
-          //Session::set('email', $email);
-     // $request->session()->set('email', $email);
-          //$checkSession = $request->session()->set('email', $email);
-          
-          /*if ($checkSession) {
-              
-              //return redirect('/api/posts');
-              return 'true';
-              
-          } else  {
-              
-              //return redirect('/api/login');
-              //return false;
-              return 'false';
-          }
-
-       // }*/
-      //return $email;
-      //return $request->session()->all();
